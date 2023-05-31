9642cd86725114f753315ace42f33c17def5c9f8
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-05-31 14:54:58 +0200

    2

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -5,7 +5,8 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     function handelClick(e) {
        const label = e.target.name;        
         onLeaveFeedback(label)
-    }
+    };
+    
     const OptionBtns = options.map((option) => {
         return <button key={option} type="button" className={css.button} name={option} onClick={handelClick}>{option}</button>
     })
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,9 +1,10 @@
-import css from './Section.module.css' 
+import css from './Section.module.css';
 
-export const Section = (props) => {
-      console.log(props)
-    return <section className={css.section}>
-        <h2 className='title'>{props.title}</h2>
-        {props.children}
+export const Section = props => {
+  return (
+    <section className={css.section}>
+      <h2 className="title">{props.title}</h2>
+      {props.children}
     </section>
-}
+  );
+};
